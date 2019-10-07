076f832e16e797e9ae8c4c716108f9f231c970ed
Your Name In Single Quotes.jumana <your email here in single quotes.mjumana28@yahoo.com>
2019-10-07 20:58:48 +0300

    edit

---
+++ hawamdeh/hawamdeh.js
@@ -0,0 +1,71 @@
+'use strict';
+var love = prompt('Is the last name of \"jumana\" is \"hawamdeh\"?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+   alert('Great answer');
+} else if (love == 'y') {
+   alert('Great answer');
+} else if (love == 'no') {
+   alert('Oooh, wrong annswer');
+} else if (love == 'n') {
+   alert('Oooh, wrong annswer');
+}
+else {
+   alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+var love = prompt('Is jumana age 23 years?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+   alert('Great one, keep going');
+} else if (love == 'y') {
+   alert('Great one, keep going');
+} else if (love == 'no') {
+   alert('WHAT DID YOU SAY!, Do you think I am at the age of your father, Certainly wrong answer.');
+} else if (love == 'n') {
+   alert('WHAT DID YOU SAY!, Do you think I am at the age of your father, Certainly wrong answer.');
+}
+else {
+   alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+var love = prompt('Has jumana studied at the ttu?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+   alert('Wow, Great answer');
+} else if (love == 'y') {
+   alert('Wow, Great answer');
+} else if (love == 'no') {
+   alert('thats rong ');
+} else if (love == 'n') {
+   alert('thats rong');
+}
+else {
+   alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+var love = prompt('do you think jumana loves football?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+   alert('Oooh, wrong annswer, sorry about that!');
+} else if (love == 'y') {
+   alert('Oooh, wrong annswer, sorry about that!');
+} else if (love == 'no') {
+   alert('of course i dont' );
+} else if (love == 'n') {
+   alert('of course i dont');
+}
+else {
+   alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
+var love = prompt('do jumana love cooking ?')
+love = love.toLowerCase();
+if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
+   alert('Of course i do');
+} else if (love == 'y') {
+   alert('Of course i do');
+} else if (love == 'no') {
+   alert('Sorry about that, wrong answer');
+} else if (love == 'n') {
+   alert('Sorry about that, wrong answer');
+}
+else {
+   alert('Wrong answer, please enter \"yes\" OR \"no\"');
+}
