ac0778dc527fed85cea35e8d719aa493342fd157
Your Name In Single Quotes.jumana <your email here in single quotes.mjumana28@yahoo.com>
2019-10-11 23:10:04 +0300

    lab4

--- hawamdeh/hawamdeh.js
+++ hawamdeh/hawamdeh.js
@@ -1,19 +1,19 @@
-'use strict';
+// 'use strict';
 // var love = prompt('Is the last name of \"jumana\" is \"hawamdeh\"?')
-// love = love.toLowerCase();
+//  love = love.toLowerCase();
 // if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
 //    alert('Great answer');
-// } else if (love == 'y') {
-//    alert('Great answer');
-// } else if (love == 'no') {
-//    alert('Oooh, wrong answer');
-// } else if (love == 'n') {
-//    alert('Oooh, wrong answer');
-// }
-// else {
-//    alert('Wrong answer, please enter \"yes\" OR \"no\"');
-// }
-// var love = prompt('Is jumana age 23 years?')
+//  } else if (love == 'y') {
+//     alert('Great answer');
+//  } else if (love == 'no') {
+//     alert('Oooh, wrong answer');
+//  } else if (love == 'n') {
+//   alert('Oooh, wrong answer');
+//  }
+//  else {
+//     alert('Wrong answer, please enter \"yes\" OR \"no\"');
+//  }
+//  var love = prompt('Is jumana age 23 years?')
 // love = love.toLowerCase();
 // if (love == 'yes') { // YES, yES, yeS, Yes, YEs, yEs,
 //    alert('Great one, keep going');
@@ -85,93 +85,79 @@ var j = [
    'Oooh, wrong annswer, sorry about that!',
    'of course i dont'
    ],['do jumana love cooking ',
-   'Of course i do',
-   'Sorry about that, wrong answer']
-]
-function res (){
+    'Of course i do',
+    'Sorry about that, wrong answer']
+ ]
+ function res (){
 
-for ( var i=0; i<5; i++) {
-   var currentj = j[i];
-   var currentp = currentj[0];
-   var responseReply = prompt (currentp);
-   responseReply = responseReply.toLowerCase();
-   var positive = currentj[1];
-   var negative = currentj [2];
+ for ( var i=0; i<5; i++) {
+    var currentj = j[i];
+    var currentp = currentj[0];
+    var responseReply = prompt (currentp);
+    responseReply = responseReply.toLowerCase();
+    var positive = currentj[1];
+    var negative = currentj [2];
 
 
-switch (responseReply) {
-   case 'yes':
-      case 'y':
-      alert(positive);
-        break;
+ switch (responseReply) {
+    case 'yes':
+       case 'y':
+       alert(positive);
+         break;
 
-        case 'no':
-           case 'n':
-              alert (negative);
-              break;
-              default:
+         case 'no':
+            case 'n':
+               alert (negative);
+               break;
+               default:
                  alert ('nooooooooooo');
 
 
+ }
+
+ }} res ();
+
+ 
+
+for (i=0 ; i<4 ;i++ ){
+   var tall = prompt (" guess what is my tall ?");
+   if (tall>154 ){
+      alert ("too high");
+   } else if (tall<154){
+      alert ("too low");
+   }else if (tall==154){
+      alert ("correct answer ")
+      mark=mark +1
+      break;
+   }
+   if (i==3){
+      alert ("my tall is 154");
+   }
+
+   
 }
 
-}} res ();
-
-// for (let j = 0; j < 3; j++) {
-//    var number = prompt('can you guess my height?');
-//  if (number == 154) {
-//    alert('correct answer, you got 1 point');
-//    break;}
-//   else if (number > 154) {
-//    alert('too high');
-//  }
-//  else  (number < 154) {
-//    alert('too short');
-
-//  }
-// }
-function awnser(){
-var length = prompt ('can you guess my height ?');
-for (let j=0 ; j<3 ; j++)
-{
-   if (length == 154){alert('correct answer , you got 1 point');}
-else if (length > 154)
-{alert(' high');}
-else if (length < 154 )
-{alert(' short');}
-else
-{alert('Correct answer ,you got 1 point');
-    break;}
-    length = prompt(' correct you guess my heigt thanks ... ');}} 
-    awnser();
-
-    function color (){
-    var color = ['red', 'black', 'banana', 'blue', 'green', 'White'];
-var userreplies = prompt('what is the color that i like ?');
-userreplies = userreplies.toLowerCase();
-for (var h = 0; h < color.length; h++){
-switch (userreplies) {
-   case 'red':
-   case 'black':
-   case 'blue':
-   case 'green':
-   case 'Yellow':
-   case 'White':
-       alert('correct answer ' );
-       var userreplies = prompt('what is the  other color that i like ?');
-       score++;
-       break;
-   default:
-           alert('wrong answer');
-           var userreplies = prompt('please reanswer the question?and choose from these choice (purple , blue , white, black, green, broun, red , orang ,grey)');
-       }
-       
-}
-alert('your score is '+ score)
-alert('the color that i like  '+ color[0] + ' ,' + color[1] + ', ' + color[2] + ', ' + color[3] + ' ,' + color[4] + ' and ' + color[5]);
-alert('Bye bye   ' + name + ', I hope that you enjoy visiting my site ');
-console.log('name', name);
-console.log('userreplay', userreply);
-console.log('age', age);
-console.log('userreplies)', userreplies);
-    }color();
+var mark=0;
+ var  child =  ['one', 'two', 'three', 'four','five','six']
+ var childhood= prompt('How many child i want to have in future?  Use letters');
+ for(var i = 0; i < child.length; i++)
+ {
+   switch(childhood)
+   {
+    case  'one':
+    case  'two':
+    case   'three':
+    case  'four':
+    case   'five':
+    case  'six':
+        alert('correct answer , keep guessing ');
+         childhood= prompt('How many child i want to have in future?');
+         mark= mark +1 ;
+      break;
+    default:
+             childhood= prompt('How many child i want to have in future?');
+             alert('Your answer wrong ,try again ');
+             break;
+   }
+   alert( 'mark' +mark);
+   }
